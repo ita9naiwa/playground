84f5d83b72712fb84fadbb8dc8ce2d7842e918bf
Hyunsung Lee <ita9naiwa@gmail.com>
2025-01-31 21:26:41 +0900

    ...

--- kernels/flash_attn.cu
+++ kernels/flash_attn.cu
@@ -1,104 +1,132 @@
 #include <torch/extension.h>
 #include <ATen/cuda/CUDAContext.h>
-
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <cmath>
 #include <cstdio>
 #include <limits>
-
 #include "c10/core/ScalarType.h"
 #include "flash_attn.h"
 #include "torch/types.h"
 
-
 const int BLOCK_SIZE = 16;
 const int DIM_SPACE = 64;
+
+__device__ inline float warpReduceMax(float val) {
+  // Suppose each warp is 32 threads, and we want separate reduce for 0..15, 16..31.
+  // laneId = 0..31
+  unsigned laneId = threadIdx.x & 31;
+  // halfId = 0 for lanes 0..15, 1 for lanes 16..31
+  unsigned halfId = laneId >> 4;
+
+  // Masks for the two half-warps:
+  unsigned maskLo = 0x0000ffff;  // active lanes 0..15
+  unsigned maskHi = 0xffff0000;  // active lanes 16..31
+  unsigned mask   = (halfId == 0) ? maskLo : maskHi;
+
+  // Each half-warp does a separate reduction on its 16 threads:
+
+  // XOR-based shuffle ignoring the other half
+  #pragma unroll
+  for (int offset = 8; offset > 0; offset >>= 1) {
+    float other = __shfl_xor_sync(mask, val, offset, 32);
+    // e.g. sum or max:
+    // val += other;
+    val = fmaxf(val, other);
+  }
+  return val;
+
+}
 template <typename scalar_t>
 __global__ void flash_attention_v1_kernel(
-  const scalar_t *Q, const scalar_t *K, const scalar_t *V,
-  scalar_t *O, scalar_t *l, scalar_t *m,
-  int B, int N, int D) {
+    const scalar_t *Q, const scalar_t *K, const scalar_t *V,
+    scalar_t *O, scalar_t *l, scalar_t *m,
+    int B, int N, int D) {
+
   __shared__ scalar_t Q_shared[BLOCK_SIZE][DIM_SPACE];
   __shared__ scalar_t K_shared[BLOCK_SIZE][DIM_SPACE];
   __shared__ scalar_t V_shared[BLOCK_SIZE][DIM_SPACE];
   __shared__ scalar_t shared_vals[BLOCK_SIZE][BLOCK_SIZE];
 
-  // Q: (B, N, D)
-  // K: (B, N, D)
-  // V: (B, N, D)
-  for(int j = 0; j < (N / BLOCK_SIZE); j++) {
-    // Load K_j, V_j into shared memory
-    for (int t = threadIdx.y; t < D; t += blockDim.y) {
-      int K_index = blockIdx.x * N * D + j * BLOCK_SIZE * D + threadIdx.x * D + t;
-      K_shared[threadIdx.x][t] = K[K_index];
-      V_shared[threadIdx.x][t] = V[K_index];
-    }
+  int tx = threadIdx.x / 16;
+  int ty = threadIdx.x % 16;
 
+  for (int j = 0; j < (N / BLOCK_SIZE); j++) {
+    for (int t = ty; t < D; t += 16) {
+      int idx = blockIdx.x * N * D + j * BLOCK_SIZE * D + tx * D + t;
+      K_shared[tx][t] = K[idx];
+      V_shared[tx][t] = V[idx];
+    }
     __syncthreads();
+
     for (int i = 0; i < (N / BLOCK_SIZE); i++) {
-      // fix current _m, _l;
       auto _m = m + blockIdx.x * N + i * BLOCK_SIZE;
       auto _l = l + blockIdx.x * N + i * BLOCK_SIZE;
-      // // Load Q_i into shared memory
-      for (int t = threadIdx.y; t < D; t += blockDim.y) {
-        int Q_index = blockIdx.x * N * D + i * BLOCK_SIZE * D + threadIdx.x * D + t;
-        Q_shared[threadIdx.x][t] = Q[Q_index];
+
+      for (int t = ty; t < D; t += 16) {
+        int idx = blockIdx.x * N * D + i * BLOCK_SIZE * D + tx * D + t;
+        Q_shared[tx][t] = Q[idx];
       }
       __syncthreads();
 
-      float S_ij = 0;
-      for (int t = 0 ; t < D; ++t)
-        S_ij += Q_shared[threadIdx.x][t] * K_shared[threadIdx.y][t];
+      float S_ij_orig = 0.0f;
+      for (int t = 0; t < D; t++) {
+        S_ij_orig += Q_shared[tx][t] * K_shared[ty][t];
+      }
 
-      // S 16 x 16 matrix, is stored across thread block (16 x 16)
-      float max_ij;
-      shared_vals[threadIdx.x][threadIdx.y] = S_ij;
-      __syncthreads();
-      if (threadIdx.y == 0) {
-        for (int t = 1; t < BLOCK_SIZE; ++t)
-          shared_vals[threadIdx.x][0] = max(shared_vals[threadIdx.x][0], shared_vals[threadIdx.x][t]);
+      float S_ij = S_ij_orig;
+      for (int offset = 8; offset > 0; offset >>= 1) {
+        float val = __shfl_down_sync(0xffff, S_ij, offset, 16);
+        S_ij = fmaxf(S_ij, val);
+      }
+
+      if (ty == 0) {
+        shared_vals[tx][0] = S_ij;
       }
       __syncthreads();
-      max_ij = shared_vals[threadIdx.x][0];
+
+      float max_ij = shared_vals[tx][0];
+      float P_ij = expf(S_ij_orig - max_ij);
+
+      shared_vals[tx][ty] = P_ij;
       __syncthreads();
-      float P_ij = exp(S_ij - max_ij);
-      __syncthreads();
-      shared_vals[threadIdx.x][threadIdx.y] = P_ij;
-      __syncthreads();
-      if (threadIdx.y == 0) {
-        for (int t = 1; t < BLOCK_SIZE; ++t)
-          shared_vals[threadIdx.x][0] += shared_vals[threadIdx.x][t];
+
+      float row_sum = P_ij;
+      for (int offset = 8; offset > 0; offset >>= 1) {
+        float val = __shfl_down_sync(0xffff, row_sum, offset, 16);
+        row_sum += val;
+      }
+
+      if (ty == 0) {
+        shared_vals[tx][0] = row_sum;
       }
       __syncthreads();
-      float l_ij = shared_vals[threadIdx.x][0];
-      float old_mi = _m[threadIdx.x];
-      float old_l = _l[threadIdx.x];
-      float new_mi = max(old_mi, max_ij);
-      float alpha = exp(old_mi - new_mi);
-      float beta = exp(max_ij - new_mi);
+
+      float l_ij = shared_vals[tx][0];
+      float old_mi = _m[tx];
+      float old_l = _l[tx];
+      float new_mi = fmaxf(old_mi, max_ij);
+      float alpha = expf(old_mi - new_mi);
+      float beta = expf(max_ij - new_mi);
       float new_l = alpha * old_l + beta * l_ij;
       __syncthreads();
-      shared_vals[threadIdx.x][threadIdx.y] = P_ij;
+
+      shared_vals[tx][ty] = P_ij;
       __syncthreads();
-      float divd = (1.0f / (1e-6f + new_l));
-      for (int t = threadIdx.y; t < D; t += blockDim.y) {
-        __syncthreads();
-        auto O_index = blockIdx.x * N * D + i * BLOCK_SIZE * D + threadIdx.x * D + t;
-        __syncthreads();
+
+      for (int t = ty; t < D; t += 16) {
+        int O_index = blockIdx.x * N * D + i * BLOCK_SIZE * D + tx * D + t;
         float O_temp = (old_l / new_l) * alpha * O[O_index];
-        __syncthreads();
-        for (int l = 0; l < BLOCK_SIZE; ++l) {
-          O_temp += (beta / new_l) * shared_vals[threadIdx.x][l] * V_shared[l][t];
+        for (int l0 = 0; l0 < BLOCK_SIZE; l0++) {
+          O_temp += (beta / new_l) * shared_vals[tx][l0] * V_shared[l0][t];
         }
-        __syncthreads();
         O[O_index] = O_temp;
-        __syncthreads();
       }
       __syncthreads();
-      if (threadIdx.y == 0) {
-        _m[threadIdx.x] = new_mi;
-        _l[threadIdx.x] = new_l;
+
+      if (ty == 0) {
+        _m[tx] = new_mi;
+        _l[tx] = new_l;
       }
       __syncthreads();
     }
@@ -106,14 +134,10 @@ __global__ void flash_attention_v1_kernel(
 }
 
 torch::Tensor flash_attention_v1(torch::Tensor Q, torch::Tensor K, torch::Tensor V) {
-  // Q: (B, H, N, D)
-  // K: (B, H, N, D)
-  // V: (B, H, N, D)
-  // B: batch size, H: number of heads, N: sequence length, D: hidden size
   auto dtype = Q.scalar_type();
-
-  if (!(dtype == torch::kFloat32 || dtype == torch::kHalf))
+  if (!(dtype == torch::kFloat32 || dtype == torch::kHalf)) {
     throw std::runtime_error("Unsupported data type");
+  }
 
   auto B = Q.size(0);
   auto H = Q.size(1);
@@ -123,14 +147,12 @@ torch::Tensor flash_attention_v1(torch::Tensor Q, torch::Tensor K, torch::Tensor
   auto options = Q.options();
   auto O = torch::zeros({B, H, N, D}, options);
   auto l = torch::zeros({B, H, N}, options);
-  // if fill_value = -infty then -infty + infty = nan error,
-  // so fill appropriate min value
   auto m = torch::full({B, H, N}, -10000.0, options);
 
-  // We just view the input tensors as 2D tensors of shape (B*H, N, D)
   auto batch_size = B * H;
-  dim3 block_size(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 block_size(BLOCK_SIZE * BLOCK_SIZE);
   dim3 grid_size(batch_size);
+
   AT_DISPATCH_FLOATING_TYPES_AND_HALF(
     Q.scalar_type(), "flash_attention_v1", [&] {
       flash_attention_v1_kernel<<<grid_size, block_size>>>(
@@ -140,11 +162,14 @@ torch::Tensor flash_attention_v1(torch::Tensor Q, torch::Tensor K, torch::Tensor
         O.data_ptr<scalar_t>(),
         l.data_ptr<scalar_t>(),
         m.data_ptr<scalar_t>(),
-        batch_size, N, D);
+        batch_size, N, D
+      );
     }
   );
+
   cudaError_t err = cudaGetLastError();
-  if (err != cudaSuccess) std::cerr << "CUDA Error: " << cudaGetErrorString(err) << std::endl;
+  if (err != cudaSuccess) {
+    std::cerr << "CUDA Error: " << cudaGetErrorString(err) << std::endl;
+  }
   return O;
 }
-
--- test_attn.py
+++ test_attn.py
@@ -3,9 +3,7 @@ import torch
 from cuda_playground import flash_attention_v1
 
 
-
-
-def test(B=1, H=1, N=32, D=64):
+def test_validation(B=1, H=1, N=32, D=64):
     Q = torch.randn(B, H, N, D).to(torch.float16).cuda()
     K = torch.randn(B, H, N, D).to(torch.float16).cuda()
     V = torch.randn(B, H, N, D).to(torch.float16).cuda()
@@ -17,7 +15,29 @@ def test(B=1, H=1, N=32, D=64):
         print("Test passed when B,H,N,D = ", B, H, N, D)
     else:
         print("Test failed when B,H,N,D = ", B, H, N, D)
-        print((att_ref / att_my).numpy())
 
-B, H, N, D = 128, 1, 64, 64
-test(B, H, N, D)
+
+B, H, N, D = 128, 32, 64, 64
+
+test_validation(B, H, N, D)
+
+
+def test_perf():
+    B, H, N, D = 128, 32, 64, 64
+    Q = torch.randn(B, H, N, D).to(torch.float16).cuda()
+    K = torch.randn(B, H, N, D).to(torch.float16).cuda()
+    V = torch.randn(B, H, N, D).to(torch.float16).cuda()
+
+    import time
+    start = time.time()
+    for i in range(100):
+        torch._scaled_dot_product_attention_math(Q, K, V, scale=1.0)[0]
+    print("Time taken for 100 iterations: ", time.time() - start)
+
+    start = time.time()
+    for i in range(100):
+        flash_attention_v1(Q, K, V)
+    print("Time taken for 100 iterations: ", time.time() - start)
+
+
+test_perf()
